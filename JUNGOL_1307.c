3ba3a0ac07a17750fdfda0d52641528c71fe089b
smileynm <77319656+smileynm@users.noreply.github.com>
2025-04-25 17:55:29 +0900

    Create JUNGOL_1307.c
    jungol_1307

---
+++ JUNGOL_1307.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int j = n * n; j >= (n * n) - n + 1; j--)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (((j - (i * n)) % 26) != 0) {
+                //printf("%c ", (j - (i * n) % 26) + 64);
+                printf("%c ", ((j - (i * n)) % 26) + 64);
+            }
+            else
+                //printf("%c ", 90);
+                printf("%c ", 90);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
