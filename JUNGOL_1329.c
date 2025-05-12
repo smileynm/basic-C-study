548e52cf764232e8b1f21afd8c847f88ca9339c2
smileynm <77319656+smileynm@users.noreply.github.com>
2025-05-12 17:21:05 +0900

    JUNGOL_1329.c
    JUNGOL_1329.c

--- JUNGOL_1329.c
+++ JUNGOL_1329.c
@@ -10,8 +10,8 @@ int main(void) {
 		return 0;
 	}
 	for (int i = 1; i <= n; i++) {
-		if (i <= (n + 1) / 2) {								// 상층부
-			for (int j = 0; j < i - 1; j++) {				// 공백 출력
+		if (i <= (n + 1) / 2) {						// 상층부
+			for (int j = 0; j < i - 1; j++) {			// 공백 출력
 				printf(" ");
 			}
 			for (int j = 1; j <= 2 * i - 1; j++) {			// 별 출력
@@ -19,11 +19,11 @@ int main(void) {
 			}
 			printf("\n");
 		}
-		else {												// 하층부
-			for (int j = 0; j < n - i; j++) {				// 공백 출력
+		else {								// 하층부
+			for (int j = 0; j < n - i; j++) {			// 공백 출력
 				printf(" ");
 			}
-			for (int j = 1; j <= 2 * (n - i) + 1; j++) {	// 별 출력
+			for (int j = 1; j <= 2 * (n - i) + 1; j++) {		// 별 출력
 				printf("*");
 			}
 			printf("\n");
